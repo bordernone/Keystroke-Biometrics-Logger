5f336c87ca15c275757d367ff26b941fdd5b38a3
bordernone <61447680+bordernone@users.noreply.github.com>
2023-06-11 00:15:15 +0400

    add support for input element

--- scripts/content.js
+++ scripts/content.js
@@ -3,30 +3,36 @@ let lastKeyDown = {};
 document.addEventListener("keydown", function (event) {
     let timestamp = new Date().getTime();
     let inputElement = document.activeElement;
-    if (inputElement && inputElement.tagName.toLowerCase() === "textarea") {
-        let key = event.key;
+    if (inputElement) {
+        const isPlainTextInput =
+            inputElement.tagName.toLowerCase() === "input" &&
+            inputElement.type === "text";
+        const isTextArea = inputElement.tagName.toLowerCase() === "textarea";
+        if (isTextArea || isPlainTextInput) {
+            let key = event.key;
 
-        // Check for key repeat
-        if (lastKeyDown[key] && timestamp - lastKeyDown[key] < 1000) {
-            return;
+            // Check for key repeat
+            if (lastKeyDown[key] && timestamp - lastKeyDown[key] < 1000) {
+                return;
+            }
+
+            lastKeyDown[key] = timestamp;
+
+            let data = {
+                type: "keydown",
+                key: key,
+                timestamp: timestamp,
+                inputId: inputElement.id,
+                inputName: inputElement.name,
+            };
+
+            // Send message to background.js
+            chrome.runtime.sendMessage({
+                from: "content",
+                subject: "record_event",
+                data: data,
+            });
         }
-
-        lastKeyDown[key] = timestamp;
-
-        let data = {
-            type: "keydown",
-            key: key,
-            timestamp: timestamp,
-            inputId: inputElement.id,
-            inputName: inputElement.name,
-        };
-
-        // Send message to background.js
-        chrome.runtime.sendMessage({
-            from: "content",
-            subject: "record_event",
-            data: data,
-        });
     }
 });
 
@@ -34,24 +40,30 @@ document.addEventListener("keyup", function (event) {
     let timestamp = new Date().getTime();
 
     let inputElement = document.activeElement;
-    if (inputElement && inputElement.tagName.toLowerCase() === "textarea") {
-        let key = event.key;
+    if (inputElement) {
+        const isPlainTextInput =
+            inputElement.tagName.toLowerCase() === "input" &&
+            inputElement.type === "text";
+        const isTextArea = inputElement.tagName.toLowerCase() === "textarea";
+        if (isTextArea || isPlainTextInput) {
+            let key = event.key;
 
-        lastKeyDown[key] = null;
+            lastKeyDown[key] = null;
 
-        let data = {
-            type: "keyup",
-            key: key,
-            timestamp: timestamp,
-            inputId: inputElement.id,
-            inputName: inputElement.name,
-        };
+            let data = {
+                type: "keyup",
+                key: key,
+                timestamp: timestamp,
+                inputId: inputElement.id,
+                inputName: inputElement.name,
+            };
 
-        // Send message to background.js
-        chrome.runtime.sendMessage({
-            from: "content",
-            subject: "record_event",
-            data: data,
-        });
+            // Send message to background.js
+            chrome.runtime.sendMessage({
+                from: "content",
+                subject: "record_event",
+                data: data,
+            });
+        }
     }
 });
